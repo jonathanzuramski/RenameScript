0a0590d4deec6bb5a7c3f9bb386bc2f2838b79e8
Jonathan Zuramski <jon@zuramskistudios.com>
2020-04-13 00:33:34 -0400

    Now will go through subdirectories

--- functions.py
+++ functions.py
@@ -1,22 +1,18 @@
-#!/usr/bin/python
-import os 
+import os
 import re
 import sys
 
-def rename(desiredName): #standard rename, does files in current directory
-        files = os.listdir(".")
 
-        print("Files processed:\n")
+def rename(desired_name):  # standard rename, does files in current directory
+    rootdir = os.getcwd()
 
-        for f in files:
-		if os.path.isdir(f): 
-			os.chdir(f)
-			rename(desiredName)
-		else: 		
-               		extensionType = f[-4:]
-                	szn = re.findall(r"([S|s]\d{2}E\d{2})",f)
-                	if len(szn) != 0:
-                        	finalName = desiredName + " " + szn[0] + extensionType
-                        	print(finalName)
-                        	os.rename(f, finalName)
-	
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            file_to_rename = os.path.join(subdir, file)
+            extensionType = file[-4:]
+            szn = re.findall(r"([S|s]\d{2}E\d{2})", file)
+            if len(szn) != 0:
+                finalName = os.path.join(
+                    subdir, desired_name + " " + szn[0] + extensionType)
+                print(finalName)
+                os.rename(file_to_rename, finalName)
